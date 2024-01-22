abce1b5cd5ff378bafeeb9bfe2fa597215ac8866
Kalmovic <joseph.ext@rallyrd.com>
2024-01-21 22:56:42 -0300

    feat: get unit temp helper funciton

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export enum TempUnitEnum {
+  Celcius = "C",
+  Fahrenheit = "F",
+}
+
+export type TempUnit = TempUnitEnum.Celcius | TempUnitEnum.Fahrenheit;
+
+export function getTempUnit(): TempUnit {
+  return (localStorage.getItem("tempUnit") as TempUnit) || TempUnitEnum.Celcius;
 }
