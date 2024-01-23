025cc967b54f3ee955b4b3ef421d9d321df65a19
Kalmovic <joseph.ext@rallyrd.com>
2024-01-23 18:15:58 -0300

    feat: improve search form

--- src/components/search-city.tsx
+++ src/components/search-city.tsx
@@ -1,16 +1,35 @@
-import React from "react";
 import { Input } from "./ui/input";
 import { Flex, IconButton } from "@radix-ui/themes";
 import { SearchIcon } from "lucide-react";
 import { getWeather } from "@/services/getWeather";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import { useGlobalLoaderStore } from "@/providers/useGlobalLoader";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Controller, useForm } from "react-hook-form";
+
+const schema = () =>
+  yup.object().shape({
+    city: yup.string().required(),
+  });
 
 export function SearchCity() {
+  const validationSchema = schema();
+
+  const { handleSubmit, formState, control } = useForm<{
+    city: string;
+  }>({
+    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      city: "",
+    },
+  });
   const navigate = useNavigate();
-  const [search, setSearch] = React.useState("");
-  const handleSubmit = async () => {
-    const cityId = search.split(" ").join("-");
+  const { setIsLoading } = useGlobalLoaderStore();
+  const onSearch = async ({ city }: { city: string }) => {
+    const cityId = city.split(" ").join("-");
+    setIsLoading(true);
     toast.info("Loading weather", {
       style: {
         bottom: "2.5rem",
@@ -27,17 +46,31 @@ export function SearchCity() {
           bottom: "2.5rem",
         },
       });
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
-    <Flex justify="between" gap="2" width="100%" align="center">
-      <Input
-        placeholder="Search City"
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <IconButton variant="surface" onClick={handleSubmit}>
-        <SearchIcon />
-      </IconButton>
-    </Flex>
+    <form
+      className="flex items-center justify-between w-full"
+      onSubmit={handleSubmit(onSearch)}
+    >
+      <Flex justify="between" gap="2" width="100%" align="center">
+        <Controller
+          control={control}
+          name="city"
+          render={({ field }) => (
+            <Input placeholder="Search City" type="text" {...field} />
+          )}
+        />
+        <IconButton
+          variant="surface"
+          type="submit"
+          disabled={!formState.isValid}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Flex>
+    </form>
   );
 }
