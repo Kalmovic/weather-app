efa88b4ed404d8d3a4dad1c3efd399e1634a5992
Kalmovic <joseph.ext@rallyrd.com>
2024-01-23 17:05:39 -0300

    update root so it is responsive

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,26 @@
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
-import { Header as Footer } from "./components/header";
+import { Flex, Theme } from "@radix-ui/themes";
+import { Footer } from "./components/footer";
 import { Outlet } from "react-router-dom";
 import "./index.css";
 import { Toaster } from "./components/ui/sonner";
+import { useMediaQuery } from "usehooks-ts";
 
 function Root() {
+  const matches = useMediaQuery("(min-width: 768px)");
+
   return (
     <Theme>
-      <Outlet />
-      <Footer />
-      <Toaster closeButton theme="light" richColors />
+      <Flex
+        className="m-auto bg-sky-50"
+        style={{
+          height: "100vh",
+        }}
+      >
+        <Outlet />
+        {matches ? null : <Footer />}
+        <Toaster closeButton theme="light" richColors />
+      </Flex>
     </Theme>
   );
 }
