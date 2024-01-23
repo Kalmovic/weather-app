5f0ee5f05b8c85dc6b7553bdcdd39c9d802c5696
Kalmovic <joseph.ext@rallyrd.com>
2024-01-23 18:16:22 -0300

    feat: rename header to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 import { SearchCity } from "./search-city";
 import { Flex } from "@radix-ui/themes";
 
-export function Header() {
+export function Footer() {
   return (
     <header className="flex items-center px-4 py-2 w-full border-t-2 fixed bottom-0 bg-white z-10">
       <Flex justify="between" gap="2" width="100%">
