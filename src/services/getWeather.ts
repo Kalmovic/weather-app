79ffc03223019b2e161f5096d364f3954dbfab8c
Kalmovic <joseph.ext@rallyrd.com>
2024-01-24 17:23:45 -0300

    undo

--- src/services/getWeather.ts
+++ src/services/getWeather.ts
@@ -131,7 +131,7 @@ export interface WeatherResponse {
 export async function getWeather(cityId: string) {
   const cityName = cityId.split("-").join(" ");
   const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://api.weatherapi.com/v1/forecast.json?key=cc4874072fe84a688bb143039242401&q=${cityName}&aqi=no&days=7&aqi=no&alerts=no`,
+    `https://api.weatherapi.com/v1/forecast.json?key=cc4874072fe84a688bb143039242401&q=${cityName}&aqi=no&days=7&aqi=no&alerts=no`,
     {
       headers: {
         "Cache-Control": "max-age=20",
